5b8b03dc1e3937b0e8007dfed7d03fcdf5f62f97
sfomuseumbot <sfomuseumbot@localhost>
2023-09-19 16:31:30 -0700

    update deps

--- TextEmbossServer/ViewController.swift
+++ TextEmbossServer/ViewController.swift
@@ -7,40 +7,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        
-        let logger = Logger(label: "org.sfomuseum.text-emboss-grpc-server")
+        let logger = Logger(label: "org.sfomuseum.text-emboss-server")
         let host = "localhost"
         let port = 1234
         
-        /*
-        let threads = 1
-                
-        let s = TextEmbossGRPC.GRPCServer(logger: logger, threads: threads)
+        var grpc = true
         
-        func Run() throws {
-            Task {
-                try await s.Run(host: host, port: port)
+        if grpc {
+                                
+            let threads = 1
+            
+            let s = TextEmbossGRPC.GRPCServer(logger: logger, threads: threads)
+            
+            func Run() throws {
+                Task {
+                    try await s.Run(host: host, port: port)
+                }
             }
-        }
-        
-        do {
-            try Run()
-        } catch {
-            fatalError("SAD")
-        }
-        
-        */
-        
-        let max_size = 10000000
-        
-        let s = TextEmbossHTTP.HTTPServer(logger: logger, max_size: max_size)
-        
-        do {
-            try s.Run(host: host, port: port)
-        } catch {
-            fatalError("SAD")
+            
+            do {
+                try Run()
+            } catch {
+                fatalError("SAD")
+            }
+            
+        } else {
+            
+            let max_size = 10000000
+            
+            let s = TextEmbossHTTP.HTTPServer(logger: logger, max_size: max_size)
+            
+            do {
+                try s.Run(host: host, port: port)
+            } catch {
+                fatalError("SAD")
+            }
         }
         
     }
